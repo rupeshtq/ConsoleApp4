2e749110e4431d76d49895a275e6c15fb3c8c6e1
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-01-28 19:45:23 +0530

    Add files via upload

---
+++ FunctionDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+//function without return type 
+namespace FunctionDemo
+{
+    /*  class Program
+      {
+          public void SHOW()
+          {
+              Console.WriteLine("THIS IS NON PARAMETER FUNCTION");
+          }
+          static void Main(string[] args)
+          {
+
+              Program p = new Program();
+              p.SHOW();
+
+
+          }
+      }
+  */
+
+    //using parameter but no return type 
+
+    class program
+    {
+        public void show (String massage)
+        {
+            Console.WriteLine("HELLO" + massage);
+
+        }
+        static void Main(string[] args)
+        {
+            program P = new program();
+            P.show("RUPESH ");//CALLIG FUNCTION
+            
+
+            
+        }
+    }
+
+}
