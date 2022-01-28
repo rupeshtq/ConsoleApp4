d9d068ed924faacd9adb34b80906e7e6b33ccbda
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-01-28 19:47:06 +0530

    Add files via upload

---
+++ object/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace cobject
+{
+    class date
+    {
+        public int day, month, year;
+    }
+    class maniplutedate
+    {
+        public void acceptdate(date d)
+        {
+            Console.WriteLine("ente day");
+            d.day = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the  month ");
+            d.month = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter year ");
+            d.year = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+        }
+
+        public void Printdate(date d)
+        {
+            Console.WriteLine(d.day + "/" + d.month + "/" + d.year);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            date d = new date();
+
+            maniplutedate obj = new maniplutedate();
+
+            obj.acceptdate(d);
+            obj.Printdate(d);
+
+
+
+        }
+    }
+}
