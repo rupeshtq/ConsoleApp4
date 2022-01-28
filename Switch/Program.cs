7a83b0df71dd1c302bdd60d0e4efe8ed7fedd82a
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-01-28 19:39:30 +0530

    Add files via upload

---
+++ Switch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char grade;
+            Console.WriteLine("enter grade");
+            grade = Convert.ToChar(Console.ReadLine());
+            switch (grade)
+            {
+                case 'A':
+                    Console.WriteLine("excellent");
+                    break;
+
+                case 'B':
+                    Console.WriteLine("very good ");
+                    break;
+
+                case 'C':
+                    Console.WriteLine("good");
+                    break;
+                default:
+                    Console.WriteLine("invalid grade");
+                    break;
+            }
+
+
+
+
+
+        }
+
+    }
+}
+
