34f6dc63a3f96ace1c2310cc474cae7b91754144
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-01-28 19:37:16 +0530

    Add files via upload

---
+++ ArrayDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ArrayDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 30, 40, 50 };//Declaration and Initialization of array  
+
+            //traversing array  
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
