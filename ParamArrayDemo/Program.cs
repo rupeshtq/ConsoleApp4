4fe1435aa1ae34e924c9cae7236e467736a377b6
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-01-28 19:48:30 +0530

    Add files via upload

---
+++ ParamArrayDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ParamArrayDemo
+{
+    class Program
+    {
+
+        public void Show(params int [] val)//params parameter
+        {
+            for(int i=0;i<val.Length;i++)
+            {
+                Console.WriteLine(val[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            p1.Show(1, 3, 3, 5, 2, 12, 4);
+
+
+        }
+    }
+}
