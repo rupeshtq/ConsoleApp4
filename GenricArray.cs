bfe8e4ffc706d6a409c6becedd233fb7a1faee04
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-02-11 22:06:36 +0530

    Add files via upload

---
+++ GenricArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GenricArrayy
+{
+
+    
+
+    public class Demo
+    {
+
+        
+        public void Display<TypeOfValue>(string msg, TypeOfValue value)
+        {
+            Console.WriteLine("{0}:{1}", msg, value);
+        }
+    }
+
+   
+    public class Example
+    {
+
+       
+        public static int Main()
+        {
+
+           
+            Demo p = new Demo();
+
+            
+            p.Display<int>("Integer", 122);
+            p.Display<char>("Character", 'H');
+            p.Display<double>("Decimal", 255.67);
+            return 0;
+        }
+    }
+}
