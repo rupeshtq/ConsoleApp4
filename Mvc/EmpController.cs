a15413a7c551cb783a65118b40ae83c3161fc381
rupeshtq <88393334+rupeshtq@users.noreply.github.com>
2022-02-16 23:36:59 +0530

    Add files via upload

---
+++ Mvc/EmpController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExample1.Controllers
+{
+    public class EmpController : Controller
+    {
+        // GET:
+        //Emp
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection collection)
+        {
+            string name = collection["txtname"].ToString();
+            Session["name"] = name;
+            return View();
+        }
+    }
+}
